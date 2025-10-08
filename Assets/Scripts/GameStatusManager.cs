488088c938548269da1f06f8b0f6f505681a2817
Ren Tamdang <tamdangren@gmail.com>
2025-10-08 23:26:00 +0800

    Losing triggers scene switch to lose scene

--- Assets/Scripts/GameStatusManager.cs
+++ Assets/Scripts/GameStatusManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStatusManager : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class GameStatusManager : MonoBehaviour
         {
             Debug.Log("Game Over");
             // Implement game over logic here (e.g., stop the game, show game over screen, etc.)
+            SceneManager.LoadScene("LoseScene");
         }
     }
 }
