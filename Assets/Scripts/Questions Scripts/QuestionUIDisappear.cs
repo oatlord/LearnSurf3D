21fc7197231ad63e2ca7cfd1634f86f6b43c3e44
Ren Tamdang <tamdangren@gmail.com>
2025-10-27 13:51:29 +0800

    Question UI created, appears and disappears through triggers

---
+++ Assets/Scripts/Questions Scripts/QuestionUIDisappear.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionUIDisappear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CanvasGroup questionCanvas;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            questionCanvas.alpha = 0;
+        }
+    }
+}
